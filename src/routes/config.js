4d9e813f4b27591ca6053efa6647cfa483dc87e2
zhengxinxing <zhengxinxing@rd.netease.com>
2019-03-29 00:45:25 +0800

    feat:  可遇后台 菜单配置

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,73 +1,118 @@
 export default {
     menus: [ // 菜单相关路由
-        { key: '/app/dashboard/index', title: '首页', icon: 'mobile', component: 'Dashboard' },
-        {
-            key: '/app/ui', title: 'UI', icon: 'scan',
+        { 
+            key: '/app/dashboard/shouldPay', title: '应付管理', icon: 'mobile', component: 'Dashboard',
             subs: [
-                { key: '/app/ui/buttons', title: '按钮', component: 'Buttons'},
-                { key: '/app/ui/icons', title: '图标', component: 'Icons'},
-                { key: '/app/ui/spins', title: '加载中', component: 'Spins'},
-                { key: '/app/ui/modals', title: '对话框', component: 'Modals'},
-                { key: '/app/ui/notifications', title: '通知提醒框', component: 'Notifications'},
-                { key: '/app/ui/tabs', title: '标签页', component: 'Tabs'},
-                { key: '/app/ui/banners', title: '轮播图', component: 'Banners'},
-                { key: '/app/ui/wysiwyg', title: '富文本', component: 'WysiwygBundle'},
-                { key: '/app/ui/drags', title: '拖拽', component: 'Drags'},
-                { key: '/app/ui/gallery', title: '画廊', component: 'Gallery'},
-                { key: '/app/ui/map', title: '地图', component: 'MapUi'},
-            ],
+                { key: '/app/ui/document', title: '供应商档案', component: 'Dashboard' },
+                { key: '/app/ui/pay', title: '应付录入', component: 'Buttons' },
+            ]
         },
         {
-            key: '/app/animation', title: '动画', icon: 'rocket',
+            key: '/app/dashboard/shouldCollect', title: '应收管理', icon: 'mobile',
             subs: [
-                { key: '/app/animation/basicAnimations', title: '基础动画', component: 'BasicAnimations'},
-                { key: '/app/animation/exampleAnimations', title: '动画案例', component: 'ExampleAnimations'},
-            ],
+                { key: '/app/ui/buttons', title: '客户档案', component: 'Dashboard' },
+                { key: '/app/ui/buttons', title: '应收录入', component: 'Dashboard' },
+            ]
         },
         {
-            key: '/app/table', title: '表格', icon: 'copy',
+            key: '/app/dashboard/pay', title: '出纳管理', icon: 'mobile',
             subs: [
-                { key: '/app/table/basicTable', title: '基础表格', component: 'BasicTable'},
-                { key: '/app/table/advancedTable', title: '高级表格', component: 'AdvancedTable'},
-                { key: '/app/table/asynchronousTable', title: '异步表格', component: 'AsynchronousTable'},
-            ],
+                { key: '/app/ui/buttons', title: '备用金管理', component: 'Dashboard' },
+                { key: '/app/ui/buttons', title: '报销管理', component: 'Dashboard' },
+                { key: '/app/ui/buttons', title: '公费管理', component: 'Dashboard' },
+            ]
         },
         {
-            key: '/app/form', title: '表单', icon: 'edit',
+            key: '/app/dashboard/data', title: '数据管理', icon: 'mobile',
             subs: [
-                { key: '/app/form/basicForm', title: '基础表单', component: 'BasicForm'},
-            ],
+                { key: '/app/ui/buttons', title: '销售数据', component: 'Dashboard' },
+                { key: '/app/ui/buttons', title: '客户数据', component: 'Dashboard' },
+                { key: '/app/ui/buttons', title: '采购数据', component: 'Dashboard' },
+            ]
         },
         {
-            key: '/app/chart', title: '图表', icon: 'area-chart',
+            key: '/app/dashboard/checkIn', title: '考勤管理', icon: 'mobile',
             subs: [
-                { key: '/app/chart/echarts', title: 'echarts', component: 'Echarts' },
-                { key: '/app/chart/recharts', title: 'recharts', component: 'Recharts' },
-            ],
+                { key: '/app/ui/buttons', title: '考勤记录', component: 'Dashboard' },
+                { key: '/app/ui/buttons', title: '班次管理', component: 'Dashboard' },
+            ]
         },
         {
-            key: '/subs4', title: '页面', icon: 'switcher',
+            key: '/app/dashboard/account', title: '账户管理', icon: 'mobile',
             subs: [
-                { key: '/login', title: '登录' },
-                { key: '/404', title: '404' },
-            ],
+                { key: '/app/ui/buttons', title: '用户管理', component: 'Dashboard' },
+            ]
         },
-        {
-            key: '/app/auth', title: '权限管理', icon: 'safety',
-            subs: [
-                { key: '/app/auth/basic', title: '基础演示', component: 'AuthBasic' },
-                { key: '/app/auth/routerEnter', title: '路由拦截', component: 'RouterEnter', auth: 'auth/testPage' },
-            ],
-        },
-        {
-            key: '/app/cssModule', title: 'cssModule', icon: 'star', component: 'Cssmodule'
-        },
-        {
-            key: '/app/extension', title: '功能扩展', icon: 'bars',
-            subs: [
-                { key: '/app/extension/queryParams', title: '问号形式参数', component: 'QueryParams', query: '?param1=1&param2=2' },
-            ],
-        },
+
+
+
+        // {
+        //     key: '/app/ui', title: '应收管理', icon: 'scan',
+        //     subs: [
+        //         { key: '/app/ui/buttons', title: '按钮', component: 'Buttons'},
+        //         { key: '/app/ui/icons', title: '图标', component: 'Icons'},
+        //         { key: '/app/ui/spins', title: '加载中', component: 'Spins'},
+        //         { key: '/app/ui/modals', title: '对话框', component: 'Modals'},
+        //         { key: '/app/ui/notifications', title: '通知提醒框', component: 'Notifications'},
+        //         { key: '/app/ui/tabs', title: '标签页', component: 'Tabs'},
+        //         { key: '/app/ui/banners', title: '轮播图', component: 'Banners'},
+        //         { key: '/app/ui/wysiwyg', title: '富文本', component: 'WysiwygBundle'},
+        //         { key: '/app/ui/drags', title: '拖拽', component: 'Drags'},
+        //         { key: '/app/ui/gallery', title: '画廊', component: 'Gallery'},
+        //         { key: '/app/ui/map', title: '地图', component: 'MapUi'},
+        //     ],
+        // },
+        // {
+        //     key: '/app/animation', title: '动画', icon: 'rocket',
+        //     subs: [
+        //         { key: '/app/animation/basicAnimations', title: '基础动画', component: 'BasicAnimations'},
+        //         { key: '/app/animation/exampleAnimations', title: '动画案例', component: 'ExampleAnimations'},
+        //     ],
+        // },
+        // {
+        //     key: '/app/table', title: '表格', icon: 'copy',
+        //     subs: [
+        //         { key: '/app/table/basicTable', title: '基础表格', component: 'BasicTable'},
+        //         { key: '/app/table/advancedTable', title: '高级表格', component: 'AdvancedTable'},
+        //         { key: '/app/table/asynchronousTable', title: '异步表格', component: 'AsynchronousTable'},
+        //     ],
+        // },
+        // {
+        //     key: '/app/form', title: '表单', icon: 'edit',
+        //     subs: [
+        //         { key: '/app/form/basicForm', title: '基础表单', component: 'BasicForm'},
+        //     ],
+        // },
+        // {
+        //     key: '/app/chart', title: '图表', icon: 'area-chart',
+        //     subs: [
+        //         { key: '/app/chart/echarts', title: 'echarts', component: 'Echarts' },
+        //         { key: '/app/chart/recharts', title: 'recharts', component: 'Recharts' },
+        //     ],
+        // },
+        // {
+        //     key: '/subs4', title: '页面', icon: 'switcher',
+        //     subs: [
+        //         { key: '/login', title: '登录' },
+        //         { key: '/404', title: '404' },
+        //     ],
+        // },
+        // {
+        //     key: '/app/auth', title: '权限管理', icon: 'safety',
+        //     subs: [
+        //         { key: '/app/auth/basic', title: '基础演示', component: 'AuthBasic' },
+        //         { key: '/app/auth/routerEnter', title: '路由拦截', component: 'RouterEnter', auth: 'auth/testPage' },
+        //     ],
+        // },
+        // {
+        //     key: '/app/cssModule', title: 'cssModule', icon: 'star', component: 'Cssmodule'
+        // },
+        // {
+        //     key: '/app/extension', title: '功能扩展', icon: 'bars',
+        //     subs: [
+        //         { key: '/app/extension/queryParams', title: '问号形式参数', component: 'QueryParams', query: '?param1=1&param2=2' },
+        //     ],
+        // },
     ],
     others: [] // 非菜单相关路由
 }
