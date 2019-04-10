f66394a77b556a3a4e2c3bdd07a403d832d86037
zhengxinxing <zhengxinxing@rd.netease.com>
2019-04-11 01:20:47 +0800

    feat:  用户管理界面布局

--- src/components/account/UserManagement.jsx
+++ src/components/account/UserManagement.jsx
@@ -14,11 +14,65 @@ class UserManagements extends Component {
         confirmDirty: false,
     };
     render() {
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 14 },
+        };
         return (
             <div className="gutter-example">
                 <BreadcrumbCustom first="账户管理" second="用户管理" />
-                <Row gutter={16}>
+
+                <Row gutter={0}>
                     <Col className="gutter-row" md={24}>
+                        <div className="gutter-box">
+                            <Card bordered={false}>
+                                <Form {...formItemLayout}>
+                                    <Row>
+                                        <Col md={8}>
+                                            <FormItem label="员工姓名" colon={false}>
+                                                <input placeholder="请输入员工姓名" />
+                                            </FormItem>
+                                        </Col>
+                                        <Col md={8}>
+                                            <FormItem label="用户类别" colon={false}>
+                                                <Select
+                                                    placeholder="请选择"
+                                                    onChange={this.handleSelectChange}
+                                                >
+                                                    <Option value="male">male</Option>
+                                                    <Option value="female">female</Option>
+                                                </Select>
+                                            </FormItem>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col md={8}>
+                                            <FormItem label="手机号码" colon={false}>
+                                                <input placeholder="请输入手机号码" />
+                                            </FormItem>
+                                        </Col>
+                                        <Col md={8}>
+                                            <FormItem label="工号" colon={false}>
+                                                <input placeholder="请输入工号" />
+                                            </FormItem>
+                                        </Col>
+                                        <Col md={2}>
+                                            <Button type="primary" htmlType="submit">查询</Button>
+                                        </Col>
+                                        <Col md={2}>
+                                            <Button type="primary" htmlType="submit">新建</Button>
+                                        </Col>
+                                        <Col md={2}>
+                                            <Button type="primary" htmlType="submit">导出</Button>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Card>
+                        </div>
+                    </Col>
+                </Row>
+                <Row gutter={16}>
+                    <Col className="gutter-row" md={24} >
                         <div className="gutter-box">
                             <Card bordered={false}>
                                 <EditableTable />
