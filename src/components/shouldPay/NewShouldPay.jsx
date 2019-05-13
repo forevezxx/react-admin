132ba73f0ab75e504bd2963680f45c507e31c654
zhengxinxing <zhengxinxing@rd.netease.com>
2019-05-13 13:14:35 +0800

    feat：图片上传

--- src/components/shouldPay/NewShouldPay.jsx
+++ src/components/shouldPay/NewShouldPay.jsx
@@ -4,7 +4,7 @@
 import React, { Component } from 'react';
 import { Card, Form, Input, Tooltip, Icon, Cascader, 
     Select, Row, Col, Checkbox, Button, 
-    Table, Menu, Tabs, Upload, message
+    Table, Menu, Tabs, Upload, message, Modal
 } from 'antd';
 import BreadcrumbCustom from '../BreadcrumbCustom';
 import { supplierAdd } from '../../axios';
@@ -31,6 +31,17 @@ class NewShouldPays extends Component {
         source: '',
 
         loading: false,
+
+        previewVisible: false,
+        previewImage: '',
+        fileList: [
+            // {
+            //     uid: -1,
+            //     name: 'xxx.png',
+            //     status: 'done',
+            //     url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            // }
+        ],
     };
     handleSelectChangeCompanyType(value){
         console.log(value)
@@ -97,40 +108,56 @@ class NewShouldPays extends Component {
         reader.addEventListener('load', () => callback(reader.result));
         reader.readAsDataURL(img);
     }
-    beforeUpload(file) {
-        const isJPG = file.type === 'image/jpeg';
-        if (!isJPG) {
-            message.error('You can only upload JPG file!');
-        }
-        const isLt2M = file.size / 1024 / 1024 < 2;
-        if (!isLt2M) {
-            message.error('Image must smaller than 2MB!');
-        }
-        return isJPG && isLt2M;
-    }
-    handleChange = (info) => {
-        debugger
-        if (info.file.status === 'uploading') {
-            this.setState({ loading: true });
-            return;
-        }
-        if (info.file.status === 'done') {
-            // Get this url from response in real world.
-            this.getBase64(info.file.originFileObj, imageUrl => this.setState({
-            imageUrl,
-            loading: false,
-            }));
-        }
-    }
-    render() {
-        const uploadButton = (
-            <div>
-                <Icon type={this.state.loading ? 'loading' : 'plus'} />
-                {/* <div className="ant-upload-text">Upload</div> */}
-            </div>
-        );
-        const imageUrl = this.state.imageUrl;
+    // beforeUpload(file) {
+    //     const isJPG = file.type === 'image/jpeg';
+    //     if (!isJPG) {
+    //         message.error('You can only upload JPG file!');
+    //     }
+    //     const isLt2M = file.size / 1024 / 1024 < 2;
+    //     if (!isLt2M) {
+    //         message.error('Image must smaller than 2MB!');
+    //     }
+    //     return isJPG && isLt2M;
+    // }
+    // handleChange = (info) => {
+    //     if (info.file.status === 'uploading') {
+    //         this.setState({ loading: true });
+    //         return;
+    //     }
+    //     if (info.file.status === 'done') {
+    //         // Get this url from response in real world.
+    //         this.getBase64(info.file.originFileObj, imageUrl => this.setState({
+    //         imageUrl,
+    //         loading: false,
+    //         }));
+    //     }
+    // }
+    handleCancel = () => this.setState({ previewVisible: false })
 
+    handlePreview = (file) => {
+        this.setState({
+        previewImage: file.url || file.thumbUrl,
+        previewVisible: true,
+        });
+    }
+
+    handleChange = ({ fileList }) => this.setState({ fileList })
+    render() {
+        // const uploadButton = (
+        //     <div>
+        //         <Icon type={this.state.loading ? 'loading' : 'plus'} />
+        //         {/* <div className="ant-upload-text">Upload</div> */}
+        //     </div>
+        // );
+        // const imageUrl = this.state.imageUrl;
+        
+        const { previewVisible, previewImage, fileList } = this.state;
+        const uploadButton = (
+        <div>
+            <Icon type="plus" />
+            <div className="ant-upload-text">Upload</div>
+        </div>
+        );
         const formItemLayout = {
             labelCol: { span: 6 },
             wrapperCol: { span: 14 },
@@ -219,17 +246,30 @@ class NewShouldPays extends Component {
                                                         label="公司照片"
                                                         // extra="33333"
                                                     >
-                                                        <Upload
+                                                        {/* <Upload
                                                             name="avatar"
                                                             listType="picture-card"
                                                             className="avatar-uploader"
                                                             showUploadList={false}
                                                             action="http://backend.delcache.com/file/upload"
-                                                            // beforeUpload={this.beforeUpload.bind(this)}
+                                                            //beforeUpload={this.beforeUpload.bind(this)}
                                                             onChange={this.handleChange.bind(this)}
                                                         >
                                                             {imageUrl ? <img src={imageUrl} alt="avatar" /> : uploadButton}
-                                                        </Upload>
+                                                        </Upload> */}
+
+                                                        <Upload
+                                                            action="http://backend.delcache.com/file/upload"
+                                                            listType="picture-card"
+                                                            fileList={fileList}
+                                                            onPreview={this.handlePreview}
+                                                            onChange={this.handleChange}
+                                                            >
+                                                            {fileList.length >= 3 ? null : uploadButton}
+                                                            </Upload>
+                                                        <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                                                        <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                                                        </Modal>
                                                     </Form.Item>
                                                     <FormItem label="合同编号" colon={false}>
                                                         <input placeholder="请输入合同编号" onChange={event => {
