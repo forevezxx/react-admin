136ec1eb1dbdad5cf32039b171671d5e0c615310
zhengxinxing <zhengxinxing@rd.netease.com>
2019-05-14 00:52:31 +0800

    feat: 代码结构优化 删除无用代码

--- src/components/index.js
+++ src/components/index.js
@@ -4,24 +4,6 @@
  */
 import Loadable from 'react-loadable';
 import Loading from './widget/Loading';
-import BasicForm from './forms/BasicForm';
-import BasicTable from './tables/BasicTables';
-import AdvancedTable from './tables/AdvancedTables';
-import AsynchronousTable from './tables/AsynchronousTable';
-import Echarts from './charts/Echarts';
-import Recharts from './charts/Recharts';
-import Icons from './ui/Icons';
-import Buttons from './ui/Buttons';
-import Spins from './ui/Spins';
-import Modals from './ui/Modals';
-import Notifications from './ui/Notifications';
-import Tabs from './ui/Tabs';
-import Banners from './ui/banners';
-import Drags from './ui/Draggable';
-import Dashboard from './dashboard/Dashboard';
-import Gallery from './ui/Gallery';
-import BasicAnimations from './animation/BasicAnimations';
-import ExampleAnimations from './animation/ExampleAnimations';
 import AuthBasic from './auth/Basic';
 import RouterEnter from './auth/RouterEnter';
 import Cssmodule from './cssmodule';
@@ -91,10 +73,7 @@ const WysiwygBundle = Loadable({ // 按需加载富文本配置
 });
 
 export default {
-    BasicForm, BasicTable, AdvancedTable, AsynchronousTable,
-    Echarts, Recharts, Icons, Buttons, Spins, Modals, Notifications,
-    Tabs, Banners, Drags, Dashboard, Gallery, BasicAnimations,
-    ExampleAnimations, AuthBasic, RouterEnter, WysiwygBundle,
+    AuthBasic, RouterEnter, WysiwygBundle,
     Cssmodule, MapUi, QueryParams,
     
     Document, NewShouldPay, WatchDocument, EditDocument, 
